59511580c82344580a4d950b5588dee31c83127a
Israel-SoaresPorto <isaelporto15@gmail.com>
2024-07-25 16:40:19 -0300

    removendo código antigo

--- js/app.js
+++ js/app.js
@@ -35,98 +35,6 @@ function showNotesCards(notesList) {
   });
 }
 
-/* function showNoteInfo(note) {
-  // Criando o elemento principal
-  let noteInfo = document.createElement("div");
-  noteInfo.setAttribute("id", "note-info");
-  noteInfo.classList.add("note_info");
-  // Adicionando o index da anotação
-  noteInfo.setAttribute("data-note-index", notesList.indexOf(note));
-
-  // Criando o elemento de título da anotação
-  let noteTitle = document.createElement("h4");
-  // Criando o elemento de descrição da anotação
-  let noteDescription = document.createElement("p");
-
-  // Criando o botão de editar anotação
-  let editButton = document.createElement("button");
-  editButton.classList.add("edit");
-  let imgEdit = document.createElement("img");
-  imgEdit.src = "assets/edit.png";
-  editButton.appendChild(imgEdit);
-  editButton.setAttribute("id", "edit-button");
-  // Adicionando o evento de editar anotação no botão
-  editButton.addEventListener("click", () => {
-    // Pegando o index da anotação
-    let index = noteInfo.getAttribute("data-note-index");
-    // Pegando a anotação para editar
-    let noteToEdit = notesList[index];
-    // Adicionando os valores da anotação no formulário de edição
-    editForm.querySelector("#edit-note-title").value = noteToEdit.title;
-    editForm.querySelector("#edit-note-description").value =
-      noteToEdit.description;
-    // Adicionando o index da anotação no modal de edição
-    editModal.setAttribute("data-note-index", index);
-    // Abrindo o modal de edição
-    editModal.showModal();
-  });
-
-  // Criando o botão de deletar anotação
-  let deleteButton = document.createElement("button");
-  deleteButton.classList.add("delete");
-  let imgDelete = document.createElement("img");
-  imgDelete.src = "assets/delete.png";
-  deleteButton.appendChild(imgDelete);
-  deleteButton.setAttribute("id", "delete-button");
-
-  // Adicionando o evento de deletar anotação no botão
-  deleteButton.addEventListener("click", () => {
-    // Pegando o index da anotação
-    let index = noteInfo.getAttribute("data-note-index");
-    // Deletando a anotação do array
-    notesList.splice(index, 1);
-    // Salvando as alterações
-    saveNotes();
-    // Mostrando as anotações
-    showNotesInfo(notesList);
-  });
-
-  // Adicionando os valores da anotação nos elementos
-  noteTitle.textContent = note.title;
-  noteDescription.textContent = note.description;
-
-  // Criando o elemento de título
-  let divTitle = document.createElement("div");
-  divTitle.classList.add("note_title");
-
-  // Criando o elemento de ações
-  let divActions = document.createElement("div");
-  divActions.classList.add("note_actions");
-
-  // Criando o elemento de conteúdo da anotação
-  let noteContent = document.createElement("div");
-  noteContent.classList.add("note_content");
-
-  // Adicionando os elementos no HTML
-  divTitle.appendChild(noteTitle);
-  divActions.appendChild(editButton);
-  divActions.appendChild(deleteButton);
-  divTitle.appendChild(divActions);
-  noteInfo.appendChild(divTitle);
-  noteContent.appendChild(noteDescription);
-  noteInfo.appendChild(noteContent);
-  notesInfo.appendChild(noteInfo);
-}
-
-// Função para mostrar as informações das anotações
-function showNotesInfo(notes) {
-  notesInfo.innerHTML = "";
-
-  notes.forEach((note) => {
-    showNoteInfo(note);
-  });
-} */
-
 // Função para salvar as anotações no localStorage
 function saveNotes() {
   localStorage.setItem("notes", JSON.stringify(notesList));
@@ -148,7 +56,7 @@ noteSearch.addEventListener("click", () => {
       note.title.toLowerCase().includes(searchValue)
     );
   });
-
+  
   showNotesCards(filteredNotes);
 });
 
